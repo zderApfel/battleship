1713f6e13e794fe366154101d8b9b42b388e0537
zderApfel <scoutt172@yahoo.com>
2020-11-04 18:29:45 -0500

    Added hit() function for ships

--- testing/gameObjects.test.js
+++ testing/gameObjects.test.js
@@ -2,20 +2,14 @@ const Ship = require("../src/gameObjects");
 
 const testShip = new Ship(3, false, false);
 
-/*test("Create a ship", () => {
-    expect(new Ship(3, false, false)).toStrictEqual(
-        {length: 3, isHit: false, isSunk: false}
-    )
-})*/
-
 test("Get a ship's health", () => {
     expect(testShip.health).toStrictEqual(
-        [{position: 1, isHit: false},{position: 2, isHit: false},{position: 3, isHit: false}]
+        [false,false,false]
     )
 })
 
-/*test("Hit a ship's position", () => {
+test("Hit a ship's position", () => {
     expect(testShip.hit(2)).toStrictEqual(
-        [{position: 1, isHit: false},{position: 2, isHit: true},{position: 3, isHit: false}]
+        [false,false,true]
     )
-})*/
+})
