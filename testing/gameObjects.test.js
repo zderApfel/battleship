602d343ff25f95aaa6011a6273668ec824e44d09
zderApfel <scoutt172@yahoo.com>
2020-11-04 18:52:38 -0500

    When a ship's health is depleted they are marked is sunk

--- testing/gameObjects.test.js
+++ testing/gameObjects.test.js
@@ -8,8 +8,21 @@ test("Get a ship's health", () => {
     )
 })
 
-test("Hit a ship's position", () => {
+test.skip("Hit a ship's position", () => {
     expect(testShip.hit(2)).toStrictEqual(
         [false,false,true]
     )
+})
+
+test.skip("Test if a ship is sunk", () => {
+    testShip.hit(0);
+    testShip.hit(1);
+    testShip.hit(2);
+    expect(testShip.isSunk).toStrictEqual(true)
+})
+
+test("Test if a ship not sunk after being hit twice", () => {
+    testShip.hit(0);
+    testShip.hit(2);
+    expect(testShip.isSunk).toStrictEqual(false)
 })
