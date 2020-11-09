fd9e565d44ba1fce40d20194e750514cde929d82
zderApfel <67031028+zderApfel@users.noreply.github.com>
2020-11-09 17:39:52 -0500

    New test

--- testing/gameObjects.test.js
+++ testing/gameObjects.test.js
@@ -1,6 +1,8 @@
 const Ship = require("../src/gameObjects");
+const Gameboard = require("../src/gameObjects");
 
 const testShip = new Ship(3, false, false);
+const testBoard = new Gameboard();
 
 test("Get a ship's health", () => {
     expect(testShip.health).toStrictEqual(
@@ -21,8 +23,12 @@ test.skip("Test if a ship is sunk", () => {
     expect(testShip.isSunk).toStrictEqual(true)
 })
 
-test("Test if a ship not sunk after being hit twice", () => {
+test.skip("Test if a ship not sunk after being hit twice", () => {
     testShip.hit(0);
     testShip.hit(2);
     expect(testShip.isSunk).toStrictEqual(false)
+})
+
+test("Adds a ship to a game board", () => {
+    expect(testBoard.placeShip)
 })
